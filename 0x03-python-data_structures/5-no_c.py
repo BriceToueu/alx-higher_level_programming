b5be7abf072ef9b7e7a0e0bad5a19c6a304f1694
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-09-16 04:20:13 -0700

    Task 5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    length = len(my_string)
+
+    l = 0
+
+    new_string = my_string[:]
+
+    for index in range(length):
+        if (my_string[index] == 'c' or my_string[index] == 'C'):
+            new_string = new_string[:(index - l)] + my_string[(index + 1):]
+            l += 1
+
+    return (new_string)
