bd09abf5e549b30b9c73b6b2858bc1aecac6bdbc
Brice Toueu <guy-brice.toueu@genesis.com>
2024-09-18 17:15:03 +0000

    Task 10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """
+    Returns a key with the biggest integer value
+    ...
+
+    Parameters
+    ----------
+    a_dictionary : dictionary
+        the given dictionary
+
+    Return:
+        If no score found, return None
+        Key with biggest integer value otherwise
+    """
+
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    fin = list(a_dictionary.keys())[0]
+    grand = a_dictionary[fin]
+    for l, t in a_dictionary.items():
+        if t > grand:
+            grand = t
+            fin = l
+    return (fin)
