c90f0b5e591fa34cdeeef09f4f26f5bee849f722
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-09-30 19:47:40 -0700

    Task 9. ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,8 +2,17 @@
 # 102-magic_calculation.py
 # Brice Toueu
 
-
 def magic_calculation(a, b):
+    """
+    Performs a calculation based on the given arguments a and b.
+
+    Args:
+        a (int): The first parameter, used in the calculation and comparison.
+        b (int): The second parameter, used in the calculation and as a fallback result.
+
+    Returns:
+        float: The result of the calculation, which is either a ** b / i or b + a.
+    """
     result = 0
     for i in range(1, 3):
         try:
