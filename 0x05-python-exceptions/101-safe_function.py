f9b2463e29edaae297474a8482e2dfac42073a00
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-09-30 19:33:43 -0700

    Task 8. Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 # 101-safe_function.py
 # Brice Toueu
 
-import sys
+# import sys
 
 
 def safe_function(fct, *args):
@@ -18,10 +18,7 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-
         return (result)
-
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-
         return (None)
