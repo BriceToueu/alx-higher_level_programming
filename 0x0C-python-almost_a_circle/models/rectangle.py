f4943b7da8a59d8818b652011fbc758a9df9c2e1
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-10-21 09:56:57 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,7 +49,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-         if type(value) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -62,7 +62,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-         if type(value) != int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -158,4 +158,3 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
-
